94ddc254ea05a3f70dfe2b45860cb81356ba99ae
GuilhermePCastro <guibkk@gmail.com>
2021-03-06 11:57:35 -0300

    Ajustando a reescrita do codigo

---
+++ resources/views/Produto/index.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Página de Clientes</title>
+  <script src="../../js/menu.js"></script>
+
+  <link href="{{ asset('css/menu.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/pg-users.css') }}">
+  <link href="https://fonts.googleapis.com/css2?family=Rhodium+Libre&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <div class="bg-blue">
+    </div>
+    <div class="bg-yellow">
+    </div>
+  </header>
+  <main class="main">
+  <nav class="sidebar">
+        <ul class="sidebar__nav">
+          <li class="nav__item hide-children">
+            <span class="item__title">
+              Cadastros
+              <img class="title__icon" src="../../svgs/arrow-down.svg" alt="arrow down">
+            </span>
+            <ul class="item__subnav">
+              <li class="subnav__item">
+                <a class="item__link" href="./clienteconsultar.php">Clientes</a>
+              </li>
+              <li class="subnav__item">
+                <a class="item__link" href="./produtoconsultar.php">Produtos</a>
+              </li>
+              <li class="subnav__item">
+                <a class="item__link" href="./usuarioconsultar.php">Usuários</a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav__item hide-children">
+            <span class="item__title">
+              Mais
+              <img
+                class="title__icon"
+                src="../../svgs/arrow-down.svg"
+                alt="arrow down"
+              />
+            </span>
+            <ul class="item__subnav">
+              <li class="subnav__item">
+                <a class="item__link" href="./logsconsultar.php">Logs</a>
+              </li>
+              <li class="subnav__item">
+                <a class="item__link" href="../backend/functions/logout.php">Logout</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <a href="../web/src/views/welcome.php">
+          <img src="../web/src/assets/images/logo.png" alt="netuno">
+        </a>
+    </nav>
+    <section class="main__page-content right-container">
+      <div class="page-content__title">
+        <h1 class="title__text">Produtos</h1>
+        <a href="../web/src/views/register-product.php">
+          <button type="button" class="title__include">
+            <img src="../../svgs/plus-square.svg" alt="+">
+            Incluir Registro
+          </button>
+        </a>
+      </div>
+
+        <!-- Mostrando mensagem na tela com a session -->
+        @if(session()->has('valido'))
+            <div class="valido">
+                {{session()->get('valido')}}
+            </div>
+        @endif
+
+            <!-- Mostrando mensagem na tela com a session -->
+        @if(session()->has('invalido'))
+            <div class="invalido">
+                {{session()->get('invalido')}}
+            </div>
+        @endif
+
+      <form class="page-content__inputs inputs-group">
+        <label class="input-container input-container-10">
+          Código
+          <input name="codigo" type="text" class="input-container__input">
+        </label>
+        <label class="input-container input-container-40">
+          Nome
+          <input name="nome" type="text" class="input-container__input">
+        </label>
+        <label class="input-container input-container-30">
+          Categoria
+          <select name="categoria" id="" required>
+            <option value="0"></option>
+            <option value="1">Boneco</option>
+            <option value="2">Carro</option>
+          </select>
+        </label>
+        <button type="submit" class="inputs__search">
+          <img src="../../svgs/search-icon.svg" alt="buscar">
+          Buscar
+        </button>
+      </form>
+
+      <table class="page-content__table"  border="0" cellpadding="0" cellspacing="0">
+        <tr align="center">
+          <th>Cód.</th>
+          <th>Nome</th>
+          <th>Categoria</th>
+          <th>Ação</th>
+        </tr>
+
+      </table>
+    </section>
+  </main>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Produtos
+Route::get('/produtos', [ProdutosController::class, 'index']) -> name('product.index');
