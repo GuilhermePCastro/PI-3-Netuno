d39a306f6d2d6cad93a87b8a89c80c1ed67705b8
GustaAzevedo <69991055+GustaAzevedo@users.noreply.github.com>
2021-05-15 21:45:11 -0300

    telas atualizada

---
+++ resources/views/prod.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/home-produto.css') }}">
+    <title>Document</title>
+</head>
+<body class="dsk-container-4x25">
+    <header>
+        <div class="header-principal bg-light d-flex align-items-center">
+            <div class=" dsk-container-4x1 sm-container-4x8 "></div>
+            <div class="logo-header bg-primary dsk-container-4x4 sm-container-4x9"></div>
+        </div>
+        <div class="header-secondary bg-light d-flex align-items-center">
+            <div class=" dsk-container-4x1"></div>
+            <div class=" dsk-container-4x4 sm-container-4x19">
+                <p class="m-0 font-weight-bold h5">Acesse sua conta</p>
+                <p class="m-0 ">home > Acessar</p>
+            </div>
+        </div>
+    </header>
+
+    <main class="dsk-container-2x25 mt-5 bg-primary py-5">
+
+    </main>
+
+</body>
+</html>
