9ca398f25897e726f77e84c1f65900689b3084f3
GustaAzevedo <69991055+GustaAzevedo@users.noreply.github.com>
2021-05-18 16:28:08 -0300

    descrição

--- resources/views/prod.blade.php
+++ resources/views/prod.blade.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/home-produto.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/prod-desc.css') }}">
+    <script src="https://kit.fontawesome.com/f9d34fff0e.js" crossorigin="anonymous"></script>
+
     <title>Document</title>
 </head>
 <body class="dsk-container-4x25">
     <header>
-        <div class="header-principal bg-light d-flex align-items-center">
+        <div class="header-principal py-5 bg-light d-flex align-items-center">
             <div class=" dsk-container-4x1 sm-container-4x8 "></div>
             <div class="logo-header bg-primary dsk-container-4x4 sm-container-4x9"></div>
         </div>
@@ -23,9 +25,41 @@
         </div>
     </header>
 
-    <main class="dsk-container-2x25 mt-5 bg-primary py-5">
+<div class=" row m-0 justify-content-center ">
+    <div class=" col-12 col-md-7 col-xl-5 produto text-center p-3">
+        <img src="https://via.placeholder.com/2000" alt="imagem">
+    </div>
 
-    </main>
+    <div class=" col-12 col-md-4 col-xl-4 produto d-flex align-items-center">
+        <div class="row m-0 w-100">
+            <div class=" col-12 text-center-sm">
+                <p class="mb-0 prod-desc-title">Camiseta Dupla Face Cobra KAI dOÔ</p>
+                <p class="mb-3 prod-desc-price">R$ 79,90</p>
+                <p class="mb-3 prod-desc-parcela">em 6x de R$ 13,92 (Sem juros)</p>
+                <div class=" mb-5 justify-content-center-sm d-flex align-items-center">
+                    <button class="m-0 btn btn-secondary h3 "><i class="fas fa-caret-left fa-2x"></i></button>
+                    <input class=" m-0 icone " disabled type="text">
+                    <button class="m-0 btn btn-secondary h3 "><i class="fas fa-caret-right fa-2x"></i></button>
+                </div>
+                <button class=" mt-5 w-100 btn btn-danger text-uppercase">Adicionar ao carrinho</button>
+            </div>
+        </div>
+    </div>
 
+    <div class="mt-4 bg-light col-11 col-md-10 col-xl-9 p-3 mb-5">
+        <div class=" row m-0 ">
+            <h3>Descrição do Produto</h3>
+            <p class=" text-desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam inventore vel,
+                tempora ipsam rerum dignissimos dolores doloribus magni iusto mollitia quo nam optio vero aperiam
+                 voluptatum voluptates corporis est vitae! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Atque error provident molestias quae nostrum illum sint, maxime cupiditate ipsam. Beatae laborum,
+                   officiis quaerat aut eos at saepe ducimus porro incidunt?</p>
+        </div>
+    </div>
+
+    <div class=" footer-dark mt-5">
+    </div>
+
+</div>
 </body>
 </html>
