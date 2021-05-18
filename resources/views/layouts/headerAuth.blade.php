8749215ccc5e426a902435efd4fdd821e80d2b9a
GuilhermePCastro <guibkk@gmail.com>
2021-05-18 08:33:59 -0300

    Imagens de login e classe

--- resources/views/layouts/headerAuth.blade.php
+++ resources/views/layouts/headerAuth.blade.php
@@ -1,13 +1,15 @@
 <header>
     <div class="header-principal bg-light d-flex align-items-center">
         <div class=" dsk-container-4x1 sm-container-4x8 "></div>
-        <div class="logo-header bg-primary dsk-container-4x4 sm-container-4x9"></div>
+        <div class="dsk-container-4x4 sm-container-4x9">
+            <img src="{{ asset( 'images/logo5.png' )}}" alt="Logo">
+        </div>
     </div>
     <div class="header-secondary bg-light d-flex align-items-center">
         <div class=" dsk-container-4x1"></div>
         <div class=" dsk-container-4x4 sm-container-4x19">
-            <p class="m-0 font-weight-bold h5">@php echo $titulo @endphp</p>
-            <p class="m-0 ">Home > @php echo $pagina @endphp</p>
+            <p class="m-0 font-weight-bold h6">@php echo $titulo @endphp</p>
+            <p class="m-0 text-sm">Home > @php echo $pagina @endphp</p>
         </div>
     </div>
 </header>
