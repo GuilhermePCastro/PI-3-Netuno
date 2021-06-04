d186158a2e6ad6b3a35581d7f824541e877acb7c
GuilhermePCastro <guibkk@gmail.com>
2021-06-03 23:12:18 -0300

    Tela de finalização

--- resources/views/carrinho/sucesso.blade.php
+++ resources/views/carrinho/sucesso.blade.php
@@ -21,24 +21,30 @@
         <div class="alert alert-danger" role="alert"> {{session()->get('error')}} </div>
     @endif
 
+    <div class="text-center my-5">
+        <p class="h1">Obrigado por comprar conosco :D</p>
+    </div>
 
     <div class=" row mx-0 justify-content-center mt-4 ">
 
         <div class="row justify-content-center">
             <div class="col-3">
-                <img src="https://img.icons8.com/color/96/000000/ok--v2.png" class="fit-image">
+                <img style="width: 130px;" class="m-auto" src="https://img.icons8.com/color/96/000000/ok--v2.png" class="fit-image">
             </div>
         </div> <br><br>
         <div class="row justify-content-center">
             <div class="col-7 text-center">
-                <h5>Compra Feita Com Sucesso</h5>
+                <p class="h3 mt-4">Compra Feita Com Sucesso!</p>
             </div>
         </div>
+    </div>
 
+    <div class="text-center my-5">
+        <p class="h4">Pedido Nº {{ $pedido->id }} gerado.</p>
     </div>
 
 
-    <div class="mt-3 footer-dark">
+    <div class="mt-3 footer-dark position-fixed">
     </div>
 
 </div>
